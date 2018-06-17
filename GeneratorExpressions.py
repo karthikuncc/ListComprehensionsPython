a53e1edf25543df9f4fb93c33ba34e0b030e6f8f
unknown <karthiky7cs1212@gmail.com>
2018-06-17 16:15:18 -0400

    second commit

---
+++ GeneratorExpressions.py
@@ -0,0 +1,5 @@
+#GE implementation
+result=(x for x in range(10))
+#Output: at 0x0000000003F7A5A0>
+#Iterable as an argument
+sum(x for x in range(10))
--- ListComp.py
+++ ListComp.py
@@ -1,24 +1,23 @@
 nums = [1,2,3,4,5,6,7,8,9,10]
 
-#List Comprehension to copy list into another list
+#List Comprehensions
 my_list = [n*n for n in nums]
-print(my_list)
-
-#List Comprehension to copy n values into Different List if n is even
 my_list = [n for n in nums if n%2==0]
-print(my_list)
-
-
-#List Comprehension Letter Number pair for each letter in 'abcd' and each number in '0123'
 my_list = [ (letter,num) for letter in 'abcd' for num in range(4)]
-print(my_list)
-
-#If and Else in LC , Elif can not used in LC
+my_list=[x for x in 'MATHEMATICS' if x in ['A','E','I','O','U']]
+#Removing vowels
+sentence = 'My name is Aarshay Jain!'
+ vowels = 'aeiou'
+result=''.join([ l for l in sentence if l not in vowels])
+#Flatten Matrix
+my_list=[x for row in matrix for x in row ]
 my_list=[x+1 if x>=5 else x+5 for x in nums]
-print(my_list)
 
-#If 
+#String to Extracting digits of a number in to a list using LC
+inp=12344
+my_list=[int(d) for d in str(inp)]
 
+#Elif can be avoided with the below style by if else
 for v in nums:
     if v == 1 :
         print('yes')
@@ -27,6 +26,11 @@ for v in nums:
             print('no')
         else:
             print('idle')
+ #Elif can be avoided with the below style by if else Using LC
 my_list=['yes' if v == 1 else 'no' if v == 2 else 'idle' for v in nums]
 for i in my_list:
     print(i)
+
+#Break statements can not be used in LC
+def oddNumbersBeforeZero(sequence):
+    return sum([1 if n%2==1  elif n==0 break for n in sequence])
